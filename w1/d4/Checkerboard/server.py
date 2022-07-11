1df16467db7506310bd7130b33778be09526a47c
bman9814 <bman9814@gmail.com>
2022-07-11 12:14:14 -0400

    morning algos (frequency table)

--- w1/d4/Checkerboard/server.py
+++ w1/d4/Checkerboard/server.py
@@ -10,8 +10,12 @@ def eightByFour():
     return render_template('8by4.html')
 
 @app.route('/<int:x>/<int:y>')
-def customSquare(x=int(4),y=int(6)):
+def customSquare(x,y):
     return render_template('custom.html', x=x,y=y)
+
+@app.route('/<int:x>/<int:y>/<color1>/<color2>')
+def customColors(x,y,color1,color2):
+    return render_template('colors.html', x=x, y=y, color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
