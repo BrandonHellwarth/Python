a4ee31fd47d6bc3bb35ac6819f6c60c3581a5185
bman9814 <bman9814@gmail.com>
2022-07-11 19:26:18 -0400

    Counter version 1

--- w2/d1/Counter/server.py
+++ w2/d1/Counter/server.py
@@ -4,14 +4,27 @@ app.secret_key = "abcdefg"
 
 @app.route('/') 
 def Counter():
-    print(request.form)
     session['num'] = 0
     return render_template('index.html')
+
+@app.route('/destroy_session')
+def destroy_session():
+    return redirect('/')
 
 @app.route('/process_click', methods = ['POST'])
 def process_click():
     session['num'] +=1
     return redirect('/newcount')
+
+@app.route('/process_plustwo', methods = ['POST'])
+def process_plustwo():
+    session['num'] +=2
+    return redirect('/newcount2')
+
+@app.route('/process_custom', methods = ['POST'])
+def process_custom():
+    session['num'] += int(request.form['custom'])
+    return redirect('/newcountcustom')
 
 @app.route('/process_reset', methods = ['POST'])
 def process_reset():
@@ -20,6 +33,15 @@ def process_reset():
 @app.route('/newcount')
 def new_count():
     return render_template('index.html')
+
+@app.route('/newcount2')
+def new_count2():
+    return render_template('index.html')
+
+@app.route('/newcountcustom')
+def newcountcustom():
+    return render_template('index.html')
+
     #THIS MUST BE ON THE BOTTOM OF THIS FILE
 if __name__=="__main__":
     app.run(debug=True)
